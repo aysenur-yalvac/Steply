21c61eb0199ab15b6043662474fa5b2f8a916018
aysenur-yalvac <aysenuryalvac04@gmail.com>
2026-08-20 13:39:58 +0300

    fix: bind onClick handler to Join button to open JoinProjectModal

---
+++ fix_jsx_modal.js
@@ -0,0 +1,14 @@
+﻿const fs = require('fs');
+let path = 'src/components/dashboard/DashboardViewSwitcher.tsx';
+let content = fs.readFileSync(path, 'utf8');
+
+content = content.replace(
+  /return \(\s*<>\s*\{\/\* "\?"\? Controls row/,
+  `return (
+    <>
+      {isJoinModalOpen && <JoinProjectModal onClose={() => setIsJoinModalOpen(false)} />}
+      {/*  Controls row`
+);
+
+fs.writeFileSync(path, content, 'utf8');
+console.log('Added JoinProjectModal to JSX');
